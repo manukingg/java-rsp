611c375527278737236bfb7dffe6886d1e6594b1
manukingg <hatbotinok@mail.ru>
2022-04-08 02:42:32 +0400

    made buttons, added DOM texts of computer and player results

--- script.js
+++ script.js
@@ -7,24 +7,24 @@ function computerPlay () {
             return "paper";
         case 2:
             return "scissors";
-    }
+    }       
 }
-
+const numberToString = {'-3':'Paper beats rock! You lose', '-2':'Rock beats scissors! You lose!', '-1':'Scissors beat paper! You lose!', 0:'Draw! Nobody wins', 1:'Rock beats scissors! You win!', 2:'Scissors beats paper! You win!', 3:'Paper beats rock! You win!Paper beats rock! You win!'}
 function playRound (playerSelection, computerSelection){
     if (playerSelection.toLowerCase() == computerSelection){
-        return "Draw! Nobody wins";
+        return 0;
     }  else if (playerSelection.toLowerCase() == "rock" && computerSelection == "paper"){
-        return "Paper beats rock! You lose!";
+        return -3;
     } else if (playerSelection.toLowerCase() == "scissors" && computerSelection == "rock"){
-        return "Rock beats scissors! You lose!";
+        return -2;
     } else if (playerSelection.toLowerCase() == "paper" && computerSelection == "scissors"){
-        return "Scissors beat paper! You lose!";
+        return -1;
     } else if (playerSelection.toLowerCase() == "rock" && computerSelection == "scissors"){
-        return "Rock beats scissors! You win!";
+        return 1;
     } else if (playerSelection.toLowerCase() == "scissors" && computerSelection == "paper"){
-        return "Scissors beats paper! You win!";
+        return 2;
     } else if (playerSelection.toLowerCase() == "paper" && computerSelection == "rock"){
-        return "Paper beats rock! You win!";
+        return 3;
     }
 }
 
@@ -32,38 +32,53 @@ function game (){
     let computerScore = 0;
     let playerScore = 0;
     let leftGames = 4;
-    for (let i = 0; i < 5; i++){
-        const playerSelection = window.prompt("What\'s Your coice?");
-        const computerSelection = computerPlay();
+    const buttons = document.querySelectorAll('button');
+    let counter = 0;
+    buttons.forEach((button) => {
+        button.addEventListener('click', function() {
+            if (counter < 5) {
+                computerSelection = computerPlay();
+                computerSelection.textContent = 'Computer\'s choice is ' + computerSelection;
+                const playerSelection = button.textContent;
+                console.log("Computer\'s selection is " + computerSelection);
+                const computerResultText = document.querySelector('.computer');
+                computerResultText.textContent = "Computer\'s selection is " + computerSelection;
+                console.log("Player\'s selection is : " + playerSelection);
+                const playerResultText = document.querySelector('.player');
+                playerResultText.textContent = "Your\'s selection is " + playerSelection;
+                const playRoundResult = playRound(playerSelection, computerSelection);
+                if (playRoundResult > 0){
+                    playerScore ++;
+                }
+                else if (playRoundResult < 0){
+                    computerScore ++;
+                }
+                console.log(numberToString[playRoundResult]);
+                console.log("Your score is " + playerScore + " and Computer\'s score is " + computerScore);
+                console.log(leftGames - counter + " games left!")
+                counter ++;
+            } else {
+                if (computerScore > playerScore){
+                    const lostAnswer = window.prompt("You lost the game! Wanna try again?");
+                    if (lostAnswer == "yes"){
+                        console.log(game());
+                    }
+                } else if (computerScore < playerScore){
+                    const wonAnswer = window.prompt("You won the game! Wanna play again?");
+                    if (wonAnswer == "yes"){
+                        console.log(game());
+                    }
+                } else {
+                    const drawAnswer = window.prompt("The result of a game is draw! Wanna play another one?");
+                    if (drawAnswer == "yes"){
+                        console.log(game());
+                    }
+                }
+            }
+            
+        });
+    });
 
-        console.log("Computer\'s selection is " + computerSelection);
-        console.log("Player\'s selection is : " + playerSelection);
-        console.log(playRound(playerSelection, computerSelection));
-        if (playRound(playerSelection, computerSelection).slice(-8) == "You win!"){
-            playerScore++;
-        }
-        else if (playRound(playerSelection, computerSelection).slice(-9) == "You lose!"){
-            computerScore++;
-        }
-        console.log("Your score is " + playerScore + " and Computer\'s score is " + computerScore);
-        console.log(leftGames - i + " games left!")
-    }
-    if (computerScore > playerScore){
-        const lostAnswer = window.prompt("You lost the game! Wanna try again?");
-        if (lostAnswer == "yes"){
-            console.log(game());
-        }
-    } else if (computerScore < playerScore){
-        const wonAnswer = window.prompt("You won the game! Wanna play again?");
-        if (wonAnswer == "yes"){
-            console.log(game());
-        }
-    } else {
-        const drawAnswer = window.prompt("The result of a game is draw! Wanna play another one?");
-        if (drawAnswer == "yes"){
-            console.log(game());
-        }
-    }
 }
 
 console.log (game());
